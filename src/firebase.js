cd9682562eebcd4af572e74d51937c37b34db3e5
Angie <caritocon@gmail.com>
2023-08-01 17:33:37 -0700

    credentials

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,6 @@ import { getAuth } from "firebase/auth";
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyChJOxca_uXW7C5-09hwGHg842iuinUrzI",
-    authDomain: "jeweltime-development.firebaseapp.com",
-    projectId: "jeweltime-development",
-    storageBucket: "jeweltime-development.appspot.com",
-    messagingSenderId: "913204107016",
-    appId: "1:913204107016:web:2574263da972b12a5c3ebd"
 };
 
 console.log("HERE IS THE APIKEY:", firebaseConfig.apiKey)
