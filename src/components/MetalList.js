3d6d01355b51b11f456f3b0b6764ddc6a081519b
Angie <caritocon@gmail.com>
2023-08-03 14:19:10 -0700

    starting with metals

---
+++ src/components/MetalList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Metal from './Metal'
+
+const MetalList = ({ metals }) => {
+    console.log("starting metal list")
+    console.log("Received metals: ", metals)
+
+    // const listOfProjects = (displayedProjects) => {
+    //     return displayedProjects.map((project) => {
+    //         return (
+    //             <Project key={project.projectId}
+    //                 project={project}
+    //                 setSelectedProject={setSelectedProject}
+    //                 deleteProject={deleteProject}
+    //             ></Project>
+    //         );
+    //     });
+    // };    
+
+};
+export default MetalList;
