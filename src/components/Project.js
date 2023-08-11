6eaecf45ea47ac239ab5df3fc12d5cd1164a6c20
Angie <caritocon@gmail.com>
2023-08-10 19:57:29 -0700

    Carousel created and working

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Carousel from 'react-bootstrap/Carousel';
 import './Project.css'
 
 const Project = ({ project, setSelectedProject, deleteProject }) => {
@@ -20,28 +22,36 @@ const Project = ({ project, setSelectedProject, deleteProject }) => {
     };
 
     return (
-        <section className="projects">
-            <h1 className='project-name'>{project.projectName}</h1>
-            <ul>
-                    {/* <label>{project.id}</label> */}
-                    <p className="project" style={{ marginTop: '20px' }}><span className="project-label">Project description:</span> {project.description}</p>
-                    <p className='project'><span className="project-label">Start date:</span> {project.startedAt}</p>
-                    <p className="project"><span className="project-label">Completion date: </span> {project.completedAt}</p>
-                    <p className="project"><span className="project-label">Hours spent:</span> {project.hoursSpent}</p>
-                    <p className="project"><span className="project-label">Materials cost:</span> ${project.materialsCost}</p>
-                    <p className="project"><span className="project-label">Materials:</span> {project.materials}</p>
-                    <p className="project"><span className="project-label">Metals:</span> {project.metals}</p>
-                    <p className="project"><span className="project-label">Gemstones:</span> {project.gemstones}</p>
-                    <p className="project"><span className="project-label">Shape:</span> {project.shape}</p>
-                    <p className="project"><span className="project-label">Jewelry type:</span>  {project.jewelryType}</p>
-                    <p className="project"><span className="project-label">Notes:</span> {project.notes}</p>
-                <li><button onClick={toggleUpdate} className="btn btn-success w-45 mt-2">Update</button></li>
-                <li>
-                    <button onClick={toggleDelete} className="btn btn-success w-47 mt-2">Delete</button>
-                    <div style={{ height: '45px' }}></div>
-                </li>
-            </ul>
-        </section>
+            <section className="projects">
+                <Card style={{ width: "32rem" }}>
+                    <Card.Body>
+                        <Card.Title>
+                            <h1 className='project-name'>{project.projectName}</h1>
+                        </Card.Title>
+                        <Card.Text>
+                            <ul>
+                                <p className="project" style={{ marginTop: '20px' }}><span className="project-label">Project description:</span> {project.description}</p>
+                                <p className='project'><span className="project-label">Start date:</span> {project.startedAt}</p>
+                                <p className="project"><span className="project-label">Completion date: </span> {project.completedAt}</p>
+                                <p className="project"><span className="project-label">Hours spent:</span> {project.hoursSpent}</p>
+                                <p className="project"><span className="project-label">Materials cost:</span> ${project.materialsCost}</p>
+                                <p className="project"><span className="project-label">Materials:</span> {project.materials}</p>
+                                <p className="project"><span className="project-label">Metals:</span> {project.metals}</p>
+                                <p className="project"><span className="project-label">Gemstones:</span> {project.gemstones}</p>
+                                <p className="project"><span className="project-label">Shape:</span> {project.shape}</p>
+                                <p className="project"><span className="project-label">Jewelry type:</span>  {project.jewelryType}</p>
+                                <p className="project"><span className="project-label">Notes:</span> {project.notes}</p>
+                            </ul>
+                            <Card.Footer>
+                                <button onClick={toggleUpdate} className="btn btn-success w-45 mt-2">Update</button>
+                                <button onClick={toggleDelete} className="btn btn-success w-47 mt-2">Delete</button>
+                            </Card.Footer>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <div style={{ height: '45px' }}></div>
+            </section>
+                    
     )
 }
 
--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Project from './Project'
+import Project from './Project';
+import Carousel from 'react-bootstrap/Carousel';
 
 const ProjectsList = ({ displayedProjects, setSelectedProject, deleteProject }) => {
     console.log("starting project list")
@@ -8,19 +9,27 @@ const ProjectsList = ({ displayedProjects, setSelectedProject, deleteProject }) 
     if (displayedProjects === undefined || displayedProjects.length === 0) {
         return <ul className="tasks__list no-bullet">No projects to show</ul>;
     } else {
-
         const listOfProjects = (displayedProjects) => {
             return displayedProjects.map((project) => {
                 return (
-                    <Project key={project.projectId}
-                        project={project}
-                        setSelectedProject={setSelectedProject}
-                        deleteProject={deleteProject}
-                    ></Project>
+                    <Carousel.Item>
+                        <Project key={project.projectId}
+                            project={project}
+                            setSelectedProject={setSelectedProject}
+                            deleteProject={deleteProject}
+                        ></Project>
+                    </Carousel.Item>
                 );
             });
         };
-        return <ul className="projects__list no-bullet">{listOfProjects(displayedProjects)}</ul>    
+        return (
+            <Carousel>
+                {listOfProjects(displayedProjects)}
+                {/* <ul className="projects__list no-bullet">{listOfProjects(displayedProjects)}</ul> */}
+            
+            </Carousel>
+            
+        ) 
     }
 };
 export default ProjectsList;
